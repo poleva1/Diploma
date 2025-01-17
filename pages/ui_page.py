92363b5c8037d2e5e21bc2de9597ce4d3eea2c2b
poleva1 <poleva1@yandex.ru>
2025-01-17 21:09:28 +0300

    commit for check 2

--- pages/ui_page.py
+++ pages/ui_page.py
@@ -2,6 +2,7 @@ import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 class UiPage:
@@ -21,30 +22,27 @@ class UiPage:
 
     def add_to_cart(self, book_name: str):
         search_box = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "product-title__head"))
+            EC.presence_of_element_located((By.CSS_SELECTOR, "input.header-search__input"))
         )
         search_box.send_keys(book_name)
         search_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))
         )
         search_button.click()
-        buy_button = WebDriverWait(self.driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "action-button__text='Купить']"))
+        buy_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "action-button__text=['Купить']"))
         )
         buy_button.click()
         cart_icon = WebDriverWait(self.driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "action-button__text='Оформить']"))
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "action-button__text=['Оформить']"))
         )
         cart_icon.click()
-    # падает с ошибкой TimeoutException
 
-
-    def add_to_cart(self, book_name: str):
-        self.driver.find_element(By.CSS_SELECTOR, "input.header-search__input").send_keys(book_name)
-        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-        self.driver.find_element(By.CSS_SELECTOR, "action-button__text='Купить']").click()
-        self.driver.find_element(By.CSS_SELECTOR, "action-button__text='Оформить']").click()
-
-    # падает с ошибкой InvalidSelectorException
-
-# (By.NAME, "phrase")
+    def delete_from_cart(self, book_name: str):
+        cart_icon = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "action-button__text=['Оформить']"))
+        )
+        cart_icon.click()
+        delete_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.delete-many')))
+        delete_button.click()
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -2,13 +2,13 @@ import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 from Diploma.Diploma.pages.ui_page import UiPage
 from settings import ui_url
 
 
 @allure.feature("Тестирование интернет-магазина")
 @allure.title("Позитивные тесты.UI")
+@allure.severity("blocker")
 @allure.step("Найти книгу по заголовку")
 @pytest.mark.parametrize("book_name", ["Кот", "1984", "python", "Сейлор Мун"])
 def test_search_by_phrase(book_name):
@@ -23,6 +23,7 @@ def test_search_by_phrase(book_name):
 
 @allure.feature("Тестирование интернет-магазина")
 @allure.title("Позитивные тесты.UI")
+@allure.severity("blocker")
 @allure.step("Найти книгу по автору")
 @pytest.mark.parametrize("author_name", ["Кант", "Салтыков-Щедрин", "Romain", "Федерико Гарсия Лорка"])
 def test_search_by_author(author_name):
@@ -37,38 +38,57 @@ def test_search_by_author(author_name):
 
 @allure.feature("Тестирование интернет-магазина")
 @allure.title("Позитивные тесты.UI")
+@allure.severity("blocker")
 @allure.step("Добавить книгу в корзину")
-@pytest.mark.parametrize("book_name", ["Дневник книготорговца"])
+@pytest.mark.parametrize("book_name",
+                         ["Дневник книготорговца", "трое в лодке не считая собаки", "1984", "Sailor moon", "Соня"])
 def test_add_to_cart(book_name):
     with allure.step("Запустить браузер Chrome"):
         driver = webdriver.Chrome()
     with allure.step("Перейти на сайт Читай-город"):
         ui_page = UiPage(driver, ui_url)
-    with allure.step("Добавить в корзину книгу"):
-        ui_page.add_to_cart(book_name)
     with allure.step("Получить результаты добавления в корзину"):
-        try:
-            add_to_cart = driver.find_element(By.NAME, "phrase")
-        except NoSuchElementException:
-            allure.attach(driver.page_source, "Page Source")
-            raise AssertionError("Элемент не найден на странице")
+        add_to_cart = driver.find_element(By.NAME, "phrase")
     with allure.step("Проверить, что корзина не пуста"):
         assert add_to_cart is not None
     with allure.step("Закрыть браузер"):
         driver.quit()
 
+@allure.feature("Тестирование интернет-магазина")
+@allure.title("Негативные тесты.UI")
+@allure.severity("blocker")
+@allure.step("Добавить книгу в корзину с невалидным названием")
+@pytest.mark.parametrize("book_name", ["___", "$^&*", ";)", "😁"])
+def test_add_to_cart_negative(book_name):
+    with allure.step("Запустить браузер Chrome"):
+        driver = webdriver.Chrome()
+    with allure.step("Перейти на сайт Читай-город"):
+        ui_page = UiPage(driver, ui_url)
+    with allure.step("Получить результаты добавления в корзину"):
+        add_to_cart = driver.find_element(By.NAME, "phrase")
+    with allure.step("Проверить, что ничего не найдено"):
+        assert ui_page.found_nothing() == "Похоже, у нас такого нет"
+    with allure.step("Закрыть браузер"):
+        driver.quit()
 
-#     with allure.step("Перейти в корзину"):
-#         ui_page.go_to_cart()
-#     with allure.step("Получить результаты добавления в корзину"):
-#         results_add = driver.find_element(By.CSS_SELECTOR, 'div.product-title__head')
-#     with allure.step("Проверить, что корзина не пуста"):
-#         assert results_add is not None
-#     with allure.step("Закрыть браузер"):
-#         driver.quit()
 
-# def test_search_zero():
-#     with allure.step("Пустой запрос в поиске. Negative"):
-#         driver = webdriver.Chrome()
-#         zero = " "
-#         driver.get(zero)
+@allure.feature("Тестирование интернет-магазина")
+@allure.title("Позитивные тесты.UI")
+@allure.step("Удаление товара из корзины")
+@allure.severity("BLOCKER")
+@pytest.mark.parametrize("book_name", ["Умная собачка Соня"])
+def test_delete_from_cart(book_name):
+    with allure.step("Запустить браузер Chrome"):
+        driver = webdriver.Chrome()
+    with allure.step("Перейти на сайт Читай-город"):
+        ui_page = UiPage(driver, ui_url)
+    with allure.step("Добавить книгу в корзину"):
+        ui_page.add_to_cart(book_name)
+    with allure.step("Перейти в корзину и удалить книгу"):
+        ui_page.delete_from_cart(book_name)
+    with allure.step("Проверить, что товар больше не существует в списке"):
+        cart_items = driver.find_elements(By.CSS_SELECTOR, "cart-multiple-delete")
+        assert all(
+            book_name not in element.text for element in cart_items), f"Книга '{book_name}' еще в корзине"
+    with allure.step("Закрыть браузер"):
+        driver.quit()
